7c3ccd3a893d7da08223d50c8f0341de082576df
Nusrath cassim <nusrathcassim1@gmail.com>
2024-06-05 21:04:51 +0530

    create template.php

---
+++ main-folder/general/template.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Boxicons CSS -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="home.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+    <div class="sidebar">
+        <div class="logo-Details">
+            <i class='bx bx-cube'></i>
+            <span class="logo_name" style="font-family: Delicious Handrawn">SelfSculp</span>
+        </div>
+        
+            <ul class="nav-links">
+                <li><a href="../Roadmap/roadmap.php">
+                        <i class='bx bx-sitemap'></i>
+                        <span class="link_name">
+                            RoadMaps
+                        </span>
+                </a></li>
+                <li><a href="../todo/todo.php">
+                    <i class='bx bxs-check-circle'></i>
+                    <span class="link_name">
+                        To.do
+                    </span>
+                </a></li>
+                <li>
+                    <div class="icon-link">
+                        <a href="#">
+                            <i class='bx bxs-heart-circle'></i>
+                                <span class="link_name">
+                                    Interests
+                                </span>
+                            </a>
+                            <i class='bx bxs-chevron-down arrow'></i>
+                    </div>
+                    <ul class="sub-menu">
+                        <li><a class="link_name" href="#">Interests</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Books</a></li>
+                        <li><a href="#">Mangas</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">
+                    <i class='bx bx-cog'></i>
+                        <span class="link_name">
+                            Setting
+                        </span>
+                </a></li>
+                <li><a href="#">
+                    <i class='bx bx-grid-alt'></i>
+                        <span class="link_name">
+                            Dashboard
+                        </span>
+                </a></li>
+        </ul>    
+    </div>
+</body>
+</html>
