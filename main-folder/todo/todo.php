47ded149f2ab4fa95032c56cc9c3ccab08934d8f
Nusrath cassim <nusrathcassim1@gmail.com>
2024-06-08 22:56:43 +0530

    create todo.php

---
+++ main-folder/todo/todo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../general/template.css">
+    <link rel="stylesheet" href="../general/home.css">
+     <!-- Include the JavaScript file -->
+     <script src="../general/common.js" defer></script>
+    <title>Todo</title>
+</head>
+<body>
+    <?php include('../general/template.php'); ?>
+
+
+    <section class="home-section">
+        <div class="home-content">
+            <i class="bx bx-menu"></i>
+            <span class="text">To-do</span>
+        </div>
+    </section>
+</body>
+</html>
